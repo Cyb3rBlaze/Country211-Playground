28153844a7b2fb5610d243af2c1599253915607b
kashyapanshulp <kashyapanshulp@gmail.com>
2023-06-02 17:15:29 +0000

    final

--- resnet_simple.py
+++ resnet_simple.py
@@ -18,10 +18,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-NAME = "resnet_simple_l2_0_epochs_100_lr_1e-3"
+NAME = "final_resnet_simple_l2_0_epochs_100_lr_3e-4"
 
-EPOCHS = 100
-lr = 1e-3
+EPOCHS = 200
+lr = 3e-4
 l2 = 0
 
 # dataset/dataloader stuff
@@ -38,13 +38,17 @@ transform = transforms.Compose([
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # normalize the images color channels
         ])
 
+val_transform = transforms.Compose([
+            transforms.Resize((224,224)),
+            transforms.ToTensor(), # convert the images to a PyTorch tensor
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # normalize the images color channels
+        ])
+
 train_set = Country211("./", "train", transform=transform)
-val_set = Country211("./", "valid", transform=transform)
-test_set = Country211("./", "test", transform=transform)
+val_set = Country211("./", "valid", transform=val_transform)
 
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
 val_loader = torch.utils.data.DataLoader(val_set, batch_size=batch_size, shuffle=True)
-test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=True)
 
 # pretrained backbone
 
@@ -95,12 +99,26 @@ optimizer = torch.optim.Adam(classification_head.parameters(), lr=lr)
 N_batch = 40
 
 train_losses = []
-# train_accuracies = []
+train_accuracies = []
 
 val_losses = []
-# val_accuracies = []
+val_accuracies = []
 
-feature_extractor.eval()
+# pulled from https://pytorch.org/ignite/generated/ignite.metrics.TopKCategoricalAccuracy.html
+def process_function(engine, batch):
+    y_pred, y = batch
+    return y_pred, y
+
+def one_hot_to_binary_output_transform(output):
+    y_pred, y = output
+    y = torch.argmax(y, dim=1)  # one-hot vector to label index vector
+    return y_pred, y
+
+engine = Engine(process_function)
+accuracy = TopKCategoricalAccuracy(k=1, output_transform=one_hot_to_binary_output_transform, device=device)
+accuracy.attach(engine, 'top_1_accuracy')
+
+# main train loop
 
 for epoch in range(EPOCHS):
     print(f"Epoch {epoch}")
@@ -129,6 +147,7 @@ for epoch in range(EPOCHS):
     classification_head.eval()
 
     temp_losses = []
+    temp_accuracies = []
 
     for i, (sample, target) in tqdm(enumerate(train_loader), total=N_batch):
         one_hot_target = nn.functional.one_hot(target, num_classes = 211).type(torch.FloatTensor).to(device)
@@ -141,15 +160,23 @@ for epoch in range(EPOCHS):
 
         temp_losses += [loss.item()]
 
+        state = engine.run([[output, one_hot_target]])
+        accuracy = state.metrics['top_1_accuracy']*100
+        temp_accuracies += [accuracy]
+
         del sample
         del target
         
         if i == N_batch-1:
             train_losses.append(np.mean(np.array(temp_losses)))
             print("Train loss: " + str(np.mean(np.array(temp_losses))))
+
+            train_accuracies.append(np.mean(np.array(temp_accuracies)))
+            print("Train accuracy: " + str(np.mean(np.array(temp_accuracies))))
             break
     
     temp_losses = []
+    temp_accuracies = []
 
     for i, (sample, target) in tqdm(enumerate(val_loader), total=len(val_loader)-1):
         one_hot_target = nn.functional.one_hot(target, num_classes = 211).type(torch.FloatTensor).to(device)
@@ -161,14 +188,27 @@ for epoch in range(EPOCHS):
         loss = crossentropy_loss(output, one_hot_target)
 
         temp_losses += [loss.item()]
+
+        state = engine.run([[output, one_hot_target]])
+        accuracy = state.metrics['top_1_accuracy']*100
+        temp_accuracies += [accuracy]
         
         if i == len(val_loader)-1:
             val_losses.append(np.mean(np.array(temp_losses)))
             print("Val loss: " + str(np.mean(np.array(temp_losses))))
 
+            val_accuracies.append(np.mean(np.array(temp_accuracies)))
+            print("Val accuracy: " + str(np.mean(np.array(temp_accuracies))))
+
             plt.plot(train_losses)
             plt.plot(val_losses)
             plt.savefig("loss_graphs/" + NAME + ".png")
+
+            plt.figure()
+
+            plt.plot(train_accuracies)
+            plt.plot(val_accuracies)
+            plt.savefig("accuracy_graphs/" + NAME + ".png")
 
             # save head
 
